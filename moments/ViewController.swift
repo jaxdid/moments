0553c9ba5c758e4265c1fef853d761023ac9cc52
BjoernWagner <bjoern.m.wagner@gmail.com>
2016-03-24 16:41:08 +0100

    WIP User Story 2 - added current location

--- moments/ViewController.swift
+++ moments/ViewController.swift
@@ -1,11 +1,30 @@
 import UIKit
 import MapKit
+import CoreLocation
 
-class ViewController: UIViewController, MKMapViewDelegate {
-
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    var locationManager = CLLocationManager()
+    
+    
     @IBOutlet var map: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation: CLLocation = locations[0]
+        let latitude = userLocation.coordinate.latitude
+        let longitude = userLocation.coordinate.longitude
+        let latDelta: CLLocationDegrees = 0.001
+        let longDelta: CLLocationDegrees = 0.001
+        let span: MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+        let location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        let region: MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        map.setRegion(region, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
