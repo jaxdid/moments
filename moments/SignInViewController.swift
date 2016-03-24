e2deee1645baebcd7de00dee2412ff94f06d13af
Alain Lemaire <jaxdid@gmail.com>
2016-03-24 20:33:45 +0100

    WIP Facebook authentication - successful connection and callback to Facebook login

---
+++ moments/SignInViewController.swift
@@ -0,0 +1,61 @@
+//
+//  SignInViewController.swift
+//  moments
+//
+//  Created by Alain on 3/24/16.
+//  Copyright © 2016 moments. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FBSDKCoreKit
+import FBSDKLoginKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func SignIn(sender: UIButton) {
+        let ref = Firebase(url: "https://makersmoments.firebaseio.com")
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logInWithReadPermissions(["email"], handler: {
+            (facebookResult, facebookError) -> Void in
+            if facebookError != nil {
+                print("Facebook login failed. Error \(facebookError)")
+            } else if facebookResult.isCancelled {
+                print("Facebook login was cancelled.")
+            } else {
+                let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
+                ref.authWithOAuthProvider("facebook", token: accessToken,
+                    withCompletionBlock: { error, authData in
+                        if error != nil {
+                            print("Login failed. \(error)")
+                        } else {
+                            print("Logged in! \(authData)")
+                        }
+                })
+            }
+        })
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
