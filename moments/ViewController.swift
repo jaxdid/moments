8522bee0307bf5ea37f69f3aac5dd3f1b8d7782c
BjoernWagner <bjoern.m.wagner@gmail.com>
2016-03-24 11:00:45 +0100

    Initial Commit

---
+++ moments/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  moments
+//
+//  Created by Björn Wagner on 24.03.16.
+//  Copyright © 2016 moments. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
