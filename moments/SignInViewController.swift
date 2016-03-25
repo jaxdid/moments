bdde6427e2d613bbe522eb60432a9e4d95ef1dbb
Tom Pickard <tcpickard94@gmail.com>
2016-03-25 12:09:57 +0000

    Fixed indentation and removed unneccessary comments

--- moments/SignInViewController.swift
+++ moments/SignInViewController.swift
@@ -15,13 +15,10 @@ class SignInViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     @IBAction func SignIn(sender: UIButton) {
@@ -47,16 +44,4 @@ class SignInViewController: UIViewController {
             }
         })
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
