1daaf86220815f70124541bf9f5b45c2d0861c39
Tom Pickard <tcpickard94@gmail.com>
2016-03-25 11:38:21 +0000

    Removed empty test

--- momentsTests/momentsTests.swift
+++ momentsTests/momentsTests.swift
@@ -19,16 +19,4 @@ class momentsTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testSignIn() {
-        
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
