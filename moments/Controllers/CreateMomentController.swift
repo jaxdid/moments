e2a696d7e5beeabcb7235891139fc0c4be2ea207
Reiss Johnson <reissjohnson@me.com>
2016-03-26 15:21:26 +0000

    WIP adding a moment

--- moments/Controllers/CreateMomentController.swift
+++ moments/Controllers/CreateMomentController.swift
@@ -1,13 +1,49 @@
 import UIKit
 
-class CreateMomentController: UIViewController {
+class CreateMomentController: UIViewController, UIPickerViewDelegate {
     var latitude: Double!
     var longitude: Double!
     
-    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var pickerView: UIPickerView!
     
-    
-    @IBAction func click(sender: AnyObject) {
-        label.text = "n\(latitude)"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        pickerView.delegate = self
     }
-}
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 2
+    }
+    
+    
+    func pickerView(pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusingView view: UIView?) -> UIView {
+        
+        
+        var myImageView = UIImageView()
+        
+        switch row {
+        case 0:
+            myImageView = UIImageView(image: UIImage(named:"grinning_face"))
+        case 1:
+            myImageView = UIImageView(image: UIImage(named:"joy"))
+        default:
+            myImageView.image = nil
+            
+            return myImageView
+        }
+        return myImageView
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+    }
+    }
+    
+
+    
+
