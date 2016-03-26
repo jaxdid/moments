4da14df602d18ce24ae8e92499a90333d677d47a
BjoernWagner <bjoern.m.wagner@gmail.com>
2016-03-26 13:52:59 +0100

    WIP US#4 Button recenters map on user's location, needs to be refactored

--- moments/ViewController.swift
+++ moments/ViewController.swift
@@ -3,7 +3,9 @@ import MapKit
 import CoreLocation
 
 class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    var manager: OneShotLocationManager?
     let locationManager = CLLocationManager()
+    var userLocation: CLLocation!
     
     @IBOutlet var map: MKMapView!
     override func viewDidLoad() {
@@ -30,5 +32,24 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    @IBAction func centerLocation(sender: UIButton) {
+        manager = OneShotLocationManager()
+        manager!.fetchWithCompletion {location, error in
+            // fetch location or an error
+            if let userLocation = location {
+                let latitude = userLocation.coordinate.latitude
+                let longitude = userLocation.coordinate.longitude
+                let latDelta: CLLocationDegrees = 0.001
+                let longDelta: CLLocationDegrees = 0.001
+                let span: MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+                let location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+                let region: MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+                self.map.setRegion(region, animated: true)
+                self.map.showsUserLocation = true
+            } else if let err = error {
+                print(err.localizedDescription)
+            }
+        }
     }
 }
