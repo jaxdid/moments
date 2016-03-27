8460b8afd1652ff6241235f24992ea4291faf1c5
Alain Lemaire <jaxdid@gmail.com>
2016-03-27 15:51:09 +0200

    WIP US#8 - Able to print momoji unicode string and text string from 'Create Moment' form

--- moments/Controllers/CreateMomentController.swift
+++ moments/Controllers/CreateMomentController.swift
@@ -1,63 +1,65 @@
 import UIKit
 
-class CreateMomentController: UIViewController, UIPickerViewDelegate {
-    var latitude: Double!
-    var longitude: Double!
-    
-    @IBOutlet weak var pickerView: UIPickerView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        pickerView.delegate = self
-    }
-    
-    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 2
-    }
-    
-    func pickerView(pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
-        return 70
-    }
-    
-    func pickerView(pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusingView view: UIView?) -> UIView {
-        
-        
-        var myImageView = UIImageView()
-        
-        switch row {
-        case 0:
-            myImageView = UIImageView(image: UIImage(named:"grinning_face"))
-        case 1:
-            myImageView = UIImageView(image: UIImage(named:"joy"))
-        default:
-            myImageView.image = nil
-            
-            return myImageView
-        }
-        return myImageView
-    }
-    
-    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        
-    }
-    
-    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
-        
-        let currentCharacterCount = textField.text?.characters.count ?? 0
-        if (range.length + range.location > currentCharacterCount){
-            return false
-        }
-        let newLength = currentCharacterCount + string.characters.count - range.length
-        return newLength <= 25
-    }
-    
-    }
-    
+class CreateMomentController: UIViewController, UIPickerViewDelegate, UITextFieldDelegate {
+  var latitude: Double!
+  var longitude: Double!
 
+  let characterLimit = 30
+  private var selectedUnicode: String = ""
+  
+  @IBOutlet weak var pickerView: UIPickerView!
+  @IBOutlet weak var textField: UITextField!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    pickerView.delegate = self
+    textField.delegate = self
+  }
+  
+  func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+    return 1
+  }
+  
+  func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    return 2
+  }
+  
+  func pickerView(pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+    return 70
+  }
+  
+  func pickerView(pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusingView view: UIView?) -> UIView {
     
+    var myImageView = UIImageView()
+    
+    switch row {
+    case 0:
+      myImageView = UIImageView(image: UIImage(named:"grinning_face"))
+      selectedUnicode = "U+1F600"
+    case 1:
+      myImageView = UIImageView(image: UIImage(named:"joy"))
+      selectedUnicode = "U+1F602"
+    default:
+      myImageView.image = nil
+    }
+
+    return myImageView
+  }
+  
+  func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+    guard let text = textField.text else { return true }
+    let newLength = text.characters.count + string.characters.count - range.length
+    return newLength <= characterLimit
+  }
+  
+  @IBAction func createMoment(sender: UIButton) {
+    //print(self.pickerView.selectedRowInComponent(0))
+    print("Unicode: \(selectedUnicode)")
+    print("Text: \(self.textField.text!)")
+    print("Location: \(latitude), \(longitude)")
+  }
+}
+
+
+
 
