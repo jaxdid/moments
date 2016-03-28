e14b761877b7e19f06214b793f98fc2e75f75678
Tom Pickard <tcpickard94@gmail.com>
2016-03-28 21:22:39 +0100

    WIP - User Story 6
    Added segue to route back to map
    Added code to display map pins

--- moments/MapController.swift
+++ moments/MapController.swift
@@ -7,27 +7,31 @@ class MapController: UIViewController, MKMapViewDelegate, CLLocationManagerDeleg
   private let momentsRef = Firebase(url: "https://makersmoments.firebaseio.com/moments")
   @IBOutlet var map: MKMapView!
   var userCoordinate: CLLocationCoordinate2D!
-  var z: String!
+  var userName: String!
   internal var locationManager: OneShotLocationManager?
   
   override func viewDidLoad() {
     super.viewDidLoad()
     momentsRef.observeAuthEventWithBlock { authData in
-        self.z = authData.providerData["displayName"]
+      self.userName = authData.providerData["displayName"] as? String
     }
     momentsRef.observeEventType(.ChildAdded, withBlock: { snapshot
-        in
-        let moment = MKPointAnnotation()
-        let a = snapshot.value.objectForKey("latitude")
-        let b = snapshot.value.objectForKey("longitude")
-        let latitude: CLLocationDegrees = (a as? Double)!
-        let longitude: CLLocationDegrees = (b as? Double)!
-        let location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
-        moment.coordinate = location
-        let c = snapshot.value.objectForKey("text")
-        moment.title = c as? String
-        moment.subtitle = self.z as? String
-        self.map.addAnnotation(moment)
+      in
+      let moment = MKPointAnnotation()
+      let a = snapshot.value.objectForKey("latitude")
+      let b = snapshot.value.objectForKey("longitude")
+      let latitude: CLLocationDegrees = (a as? Double)!
+      let longitude: CLLocationDegrees = (b as? Double)!
+      let location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+      moment.coordinate = location
+      let c = snapshot.value.objectForKey("text")
+      let momoji = snapshot.value.objectForKey("momoji") as! String
+      let cut = momoji.substringWithRange(Range<String.Index>(start: momoji.startIndex.advancedBy(2), end: momoji.endIndex.advancedBy(0)))
+      print(cut)
+      let scalar = String(Character(UnicodeScalar(Int(cut, radix: 16)!)))
+      moment.title = "\(scalar)"
+      moment.subtitle = self.userName
+      self.map.addAnnotation(moment)
     })
     self.focusMapOnUser()
   }
@@ -56,11 +60,11 @@ class MapController: UIViewController, MKMapViewDelegate, CLLocationManagerDeleg
     let region = MKCoordinateRegionMake(coordinate, MKCoordinateSpanMake(viewRadius, viewRadius))
     self.map.setRegion(region, animated: true)
   }
-
+  
   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
     if let destinationController = segue.destinationViewController as? CreateMomentController {
       destinationController.userCoordinate = userCoordinate
     }
     segue.sourceViewController
-  }  
+  }
 }
