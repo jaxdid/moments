0139f8dd65c7110e3cb16aea355c33c05bf0a0bd
Douglas Rose <douglahr@gmail.com>
2016-03-28 17:11:49 +0200

    wrote a test that mocks firebase

--- moments/mockFirebase.swift
+++ moments/mockFirebase.swift
@@ -5,33 +5,11 @@ import Firebase
 class MockFirebase: Firebase {
     
     var authError:NSError? = NSError(domain: "User authentication", code: 1, userInfo: nil)
-    var user: User!
-    var users: [User] = []
-    var current_user: User?
+    var moment: Moment!
+    var moments: [Moment] = []
+    var current_user: Moment?
     var snapshot: Snapshot?
     
-    override var authData: FAuthData? {
-        get {
-            if let authUserData = current_user {
-                return authUserData
-            } else {
-                return nil
-            }
-        }
-    }
-    
-    override func authUser(email: String!, password: String!, withCompletionBlock block: ((NSError!, FAuthData!) -> Void)!) {
-        var error = authError
-        
-        for user in users {
-            if(user.email! == email && user.password == password) {
-                current_user = user
-                error = nil
-            }
-        }
-        
-        block(error, authData)
-    }
     
     override func observeEventType(eventType: FEventType, andPreviousSiblingKeyWithBlock block: ((FDataSnapshot!, String!) -> Void)!) -> UInt {
         block(snapshot, nil)
@@ -42,23 +20,9 @@ class MockFirebase: Firebase {
         return Firebase()
     }
     
-    override func createUser(email: String!, password: String!, withCompletionBlock block: ((NSError!) -> Void)!) {
-        var error = authError
-        var exists = false
-        
-        for existingUser in users {
-            if (existingUser.email == email) {
-                exists = true
-            }
-        }
-        
-        if(!exists) {
-            user = User(email: email, password: password)
-            users += [user]
-            error = nil
-        }
-        
-        block(error)
+    func createMoment(text: String!) {
+        moment = Moment(text: text)
+        moments += [moment]
     }
     
     override func unauth() {
@@ -67,58 +31,15 @@ class MockFirebase: Firebase {
     
 }
 
-class User: FAuthData {
-    var email: String!
-    var password: String!
-    
-    init?(email: String, password: String) {
+class Moment: FMutableData{
+    var text: String!
+    init?(text: String) {
         super.init()
-        
-        if(email == "" || password == "") { return nil }
-        self.email = email
-        self.password = password
+        self.text=text
     }
     
-    override var uid: String {
-        get {
-            return "a8cfdc95-e221-4108-967a-7ac7e3d48b6d"
-        }
-    }
-    
-    override var provider: String {
-        get {
-            return "password"
-        }
-    }
-    
-    override var expires: NSNumber {
-        get {
-            return 1451558993
-        }
-    }
-    
-    override var auth: [NSObject: AnyObject] {
-        get {
-            return [ "uid": uid, "provider": provider]
-        }
-    }
-    
-    override var token: String {
-        get {
-            return "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ2IjowLCJkIjp7InByb3ZpZGVyIjoicGFzc3dvcmQiLCJ1aWQiOiJhOGNmZGM5NS1lMjIxLTQxMDgtOTY3YS03YWM3ZTNkNDhiNmQifSwiaWF0IjoxNDUxNDcyNTkzfQ.JcT_Pu7pbfjNTNrgvdct1rCG9z_sftYoRTRtUtM423Q"
-        }
-    }
-    
-    override var providerData: [NSObject: AnyObject] {
-        get {
-            return [
-                "profileImageURL": "https://secure.gravatar.com/avatar/8da15a9afec4c29790fff3a0a2b45608?d=retro",
-                "isTemporaryPassword": 0,
-                "email": email
-            ]
-        }
-    }
 }
+
 
 class Snapshot: FDataSnapshot {
     var FBref: MockFirebase!
--- momentsTests/FirebaseMockSpec.swift
+++ momentsTests/FirebaseMockSpec.swift
@@ -1,35 +1,25 @@
-//
-//  FirebaseMockSpec.swift
-//  moments
-//
-//  Created by Reiss Johnson on 28/03/2016.
-//  Copyright © 2016 moments. All rights reserved.
-//
-
 import XCTest
 import Quick
 import Nimble
 
 class FirebaseMockSpec: QuickSpec {
-    
     override func spec() {
         var ref: MockFirebase!
-        UIApplication.sharedApplication().delegate = TestingAppDelegate()
         
         describe("moments") {
-        
+            
             beforeEach {
-            ref = MockFirebase()
+                ref = MockFirebase()
             }
-        
+            
             it("lets you create a moment"){
-                ref.setValue(["text": "mockstring"])
-                ref.observeEventType(.ChildAdded, withBlock: { snapshot
-                    in
-                    print (snapshot.value.objectForKey("text"))
-                    //expect(snapshot.value.objectForKey("text")).to(equal("mockstring"))
-                })
+                
+                ref.createMoment("hello")
+                
+                expect(ref.moment.text).to(equal("hello"))
             }
         }
+        
     }
 }
+
