e74268e019d464f0235869098b069274635dc3e6
Tom Pickard <tcpickard94@gmail.com>
2016-03-28 10:40:04 +0100

    Fixed tests

--- momentsTests/ViewControllerUnitTests.swift
+++ momentsTests/ViewControllerUnitTests.swift
@@ -1,12 +1,6 @@
-//
-//  ViewControllerUnitTests.swift
-//  moments
-//
-//  Created by Tom Pickard on 26/03/2016.
-//  Copyright © 2016 moments. All rights reserved.
-//
-
 import XCTest
+import CoreLocation
+
 @testable import moments
 
 class ViewControllerUnitTests: XCTestCase {
@@ -21,15 +15,15 @@ class ViewControllerUnitTests: XCTestCase {
     
     func testValuePassedOnSegue() {
         let controller = ViewController()
-        controller.latitude = 37.89
+        controller.userLocation = CLLocation(latitude: 12, longitude: 14)
         let destinationController = CreateMomentController()
         let segue = UIStoryboardSegue(identifier: "sa",
                                       source: controller,
                                       destination: destinationController)
         controller.prepareForSegue(segue, sender: nil)
-        
-        if let latitude = controller.latitude {
-            XCTAssertEqual(37.89, latitude)
+
+        if let location:Optional = destinationController.userLocation {
+            XCTAssertEqual(controller.userLocation, location)
         } else {
             XCTFail("Arguments should be passed")
         }
