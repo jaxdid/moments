6e311618902db039b11583a1992d49b07a937ebe
Alain Lemaire <jaxdid@gmail.com>
2016-03-28 13:27:03 +0200

    Closes #8

--- moments/Controllers/CreateMomentController.swift
+++ moments/Controllers/CreateMomentController.swift
@@ -63,6 +63,5 @@ class CreateMomentController: UIViewController, UIPickerViewDelegate, UITextFiel
                   "user": self.userId]
     let momentRef = momentsRef.childByAutoId()
     momentRef.setValue(moment)
-    self.performSegueWithIdentifier("submitMoment", sender: nil)
   }
 }
