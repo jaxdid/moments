1a8bddabc6e73e34fb5b85eb7bdf9ee5cc69da7b
Reiss Johnson <reissjohnson@me.com>
2016-03-29 16:06:47 +0100

    added map view file

--- moments/MapView.swift
+++ moments/MapView.swift
@@ -3,34 +3,34 @@ import UIKit
 import MapKit
 
 extension MapController {
-  
-  func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
-    if (annotation is MKUserLocation) {
-      return nil
-    }
     
-    let reuseId = "mapAnnotation"
-    
-    var annotationView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId)
-    if annotationView == nil {
-      let customAnnotation = annotation as! MapAnnotation
-      annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
-      switch customAnnotation.momoji {
-      case "1F600":
-        annotationView?.image = UIImage(named:"grinning_face")
-      case "1F602":
-        annotationView?.image = UIImage(named:"joy")
-      default:
-        annotationView?.image = UIImage(named:"grinning_face")
-      }
-      annotationView?.canShowCallout = true
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        if (annotation is MKUserLocation) {
+            return nil
+        }
+        
+        let reuseId = "mapAnnotation"
+        
+        var annotationView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId)
+        if annotationView == nil {
+            let customAnnotation = annotation as! MapAnnotation
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            switch customAnnotation.momoji {
+            case "1F600":
+                annotationView?.image = UIImage(named:"grinning_face")
+            case "1F602":
+                annotationView?.image = UIImage(named:"joy")
+            default:
+                annotationView?.image = UIImage(named:"grinning_face")
+            }
+            annotationView?.canShowCallout = true
+        }
+        else {
+            
+            //we are re-using a view, update its annotation reference...
+            annotationView?.annotation = annotation
+        }
+        
+        return annotationView
     }
-    else {
-      
-      //we are re-using a view, update its annotation reference...
-      annotationView?.annotation = annotation
-    }
-    
-    return annotationView
-  }
 }
