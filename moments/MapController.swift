4b4c613cfd0b7197161dc7ee6d4b9320840de8c4
Alain Lemaire <jaxdid@gmail.com>
2016-03-29 14:06:58 +0200

    WIP - Created generic AnnotationView to replace marker image

--- moments/MapController.swift
+++ moments/MapController.swift
@@ -11,19 +11,62 @@ class MapController: UIViewController, MKMapViewDelegate, CLLocationManagerDeleg
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    map.delegate = self
     momentsRef.observeEventType(.ChildAdded, withBlock: { snapshot in
       let latitude = snapshot.value.objectForKey("latitude") as! Double
       let longitude = snapshot.value.objectForKey("longitude") as! Double
       let text = snapshot.value.objectForKey("text") as! String
       let momoji = snapshot.value.objectForKey("momoji") as! String
-      let scalar = String(Character(UnicodeScalar(Int(momoji, radix: 16)!)))
+      //let scalar = String(Character(UnicodeScalar(Int(momoji, radix: 16)!)))
       let moment = MapAnnotation(title: "\(text)",
                                  subtitle: snapshot.value.objectForKey("userName") as! String,
-                                 coordinate: CLLocationCoordinate2DMake(latitude, longitude))
+                                 coordinate: CLLocationCoordinate2DMake(latitude, longitude),
+                                 momoji: momoji)
+      
       self.map.addAnnotation(moment)
     })
+    
+    let test = MapAnnotation(title: "Test Anno",
+                               subtitle: "This is a test",
+                               coordinate: CLLocationCoordinate2DMake(0.0, 0.0),
+                               momoji: "1F600")
+    
+    self.map.addAnnotation(test)
+    
+    var anView:MKAnnotationView = MKAnnotationView()
+    anView.annotation = test
+    anView.image = UIImage(named:"joy")
+    anView.canShowCallout = true
+    anView.enabled = true
+    
     self.focusMapOnUser()
   }
+  
+  //=========================================================
+  func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+    if (annotation is MKUserLocation) {
+      //if annotation is not an MKPointAnnotation (eg. MKUserLocation),
+      //return nil so map draws default view for it (eg. blue dot)...
+      return nil
+    }
+    
+    let reuseId = "test"
+    
+    var anView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId)
+    if anView == nil {
+      anView = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+      anView?.image = UIImage(named:"joy")
+      anView?.canShowCallout = true
+    }
+    else {
+      //we are re-using a view, update its annotation reference...
+      anView?.annotation = annotation
+    }
+    
+    return anView
+  }
+  //=========================================================
+
   
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
